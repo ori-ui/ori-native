2a65cf0a62aca5fb68f6bc0c78bd1c6ab91c53a0
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2026-02-23 15:53:03 +0100

    add: keyed to prelude

--- src/lib.rs
+++ src/lib.rs
@@ -27,8 +27,8 @@ pub mod views {
 pub mod prelude {
     pub use crate::{
         Action, Align, App, AutoLength, BorderLayout, BuildMarker, BuildView, Color,
-        ContainerLayout, Context, Effect, Element, FlexLayout, Fraction, Justify, Layout, Length,
-        Message, Position, Proxy, View, views::*,
+        ContainerLayout, Context, Effect, Element, FlexLayout, Fraction, Justify, Keyed, Layout,
+        Length, Message, Position, Proxy, View, keyed, views::*,
     };
 
     #[allow(unused_imports)]
